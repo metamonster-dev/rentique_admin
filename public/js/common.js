594a28d30875d46ac7a76b4766648d3f1d7df3f1
신호태 <ubiquitous6666@gmail.com>
2024-10-17 21:45:40 +0900

    api 전송 함수화

--- public/js/common.js
+++ public/js/common.js
@@ -1,3 +1,4 @@
+// 검색할 때 달력 조건
 document.addEventListener('DOMContentLoaded', function () {
     const startDateInput = document.getElementById('start-date');
     const endDateInput = document.getElementById('end-date');
@@ -23,6 +24,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
+// 목록 전체 체크 박스
 if (document.querySelector('#selectAll')) {
     document.querySelector('#selectAll').addEventListener('click', function () {
         const checkboxes = document.querySelectorAll('input[name="selected_ids[]"]');
@@ -30,12 +32,14 @@ if (document.querySelector('#selectAll')) {
     });
 }
 
+// 엔터 눌렀을 때 검색
 document.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
         validateForm();
     }
 });
 
+// 검색 버튼 클릭 시 검증
 function validateForm() {
     const startDate = document.getElementById('start-date').value;
     const endDate = document.getElementById('end-date').value;
@@ -56,4 +60,51 @@ function validateForm() {
     }
 
     return true;
+}
+
+// api 전송
+function fetchRequest(url, method = 'GET', data = null, redirectUrl = null) {
+    const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content'); // CSRF 토큰을 메타 태그에서 가져옴
+
+    const headers = {
+        'X-CSRF-TOKEN': csrfToken,
+        'Content-Type': 'application/json',
+    };
+
+    let options = {
+        method: method,
+        headers: headers,
+    };
+
+    if (data) {
+        options.body = JSON.stringify(data);
+    }
+
+    return fetch(url, options)
+        .then(response => {
+            if (response.status === 422) {
+                return response.json().then(data => {
+                    let errorMessage = data.errors.memo ? data.errors.memo[0] : '입력 오류가 발생했습니다.';
+                    alert(errorMessage);
+                    throw new Error(errorMessage);
+                });
+            }
+            return response.json();
+        })
+        .then(data => {
+            alert(data.message);
+            if (data.success) {
+                if (redirectUrl) {
+                    window.location.href = redirectUrl;
+                } else {
+                    window.location.reload();
+                }
+            } else {
+                window.location.reload();
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('네트워크 오류가 발생했습니다.');
+        });
 }
