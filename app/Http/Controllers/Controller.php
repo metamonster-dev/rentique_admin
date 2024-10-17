2e7c088f796fbd5a7042dbac4cd8c8e775c969e0
신호태 <ubiquitous6666@gmail.com>
2024-10-17 18:54:32 +0900

    공용 컨트롤러 예외 처리 생성

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Log;
+
 abstract class Controller
 {
-    //
+    protected function handleException(Exception $e, $customMessage): RedirectResponse
+    {
+        Log::error($customMessage, [
+            'error_message' => $e->getMessage(),
+            'stack_trace' => $e->getTraceAsString(),
+            'file' => $e->getFile(),
+            'line' => $e->getLine(),
+        ]);
+
+        return redirect()->back()->withErrors(['error' => $customMessage]);
+    }
 }
